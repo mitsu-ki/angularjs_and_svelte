612f9cc6d97744edd892de47308aa4ac05dc939f
mitsu-ki <mitsuhiro.kine@creativesurvey.com>
2021-06-08 20:14:49 +0900

    Added sample to communicate with ancestors at Custom Event

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import svelteGreeting from "./svelte_greeting.svelte";
 import svelteButton from "./svelte_button.svelte";
+import svelteCustomEvent from "./svelte_custom_event.svelte";
 
 // 各 svelte コンポーネントを登録しておく
 window.svelteGreeting = svelteGreeting;
 window.svelteButton = svelteButton;
+window.svelteCustomEvent = svelteCustomEvent;
---
+++ src/svelte_custom_event.svelte
@@ -0,0 +1,13 @@
+<script>
+  let clickFn = function () {
+    var event = new CustomEvent("svelte:showComment", {
+      detail: {
+        text: "svelte element text",
+        key: "value",
+      },
+    });
+    document.dispatchEvent(event);
+  };
+</script>
+
+<button type="button" on:click={clickFn}> check custom event</button>
